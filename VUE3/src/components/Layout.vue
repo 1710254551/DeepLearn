fa8f535222b76ee0ae45e97838f00214d84620cb
wyunyi <wyunyi@linewell.com>
2021-12-06 18:13:26 +0800

    update: 动态菜单(半成品)

---
+++ VUE3/src/components/Layout.vue
@@ -0,0 +1,75 @@
+<template>
+  <a-layout>
+    <a-layout-sider v-model:collapsed="collapsed" :trigger="null" collapsible>
+      <div class="logo" />
+      <a-menu theme="dark" mode="inline" v-model:selectedKeys="selectedKeys">
+      </a-menu>
+    </a-layout-sider>
+    <a-layout>
+      <a-layout-header style="background: #fff; padding: 0">
+        <menu-unfold-outlined
+          v-if="collapsed"
+          class="trigger"
+          @click="() => (collapsed = !collapsed)"
+        />
+        <menu-fold-outlined
+          v-else
+          class="trigger"
+          @click="() => (collapsed = !collapsed)"
+        />
+      </a-layout-header>
+      <a-layout-content
+        :style="{
+          margin: '24px 16px',
+          padding: '24px',
+          background: '#fff',
+          minHeight: 'calc(100vh - 112px)',
+        }"
+      >
+        Content
+      </a-layout-content>
+    </a-layout>
+  </a-layout>
+</template>
+<script>
+import { defineComponent, ref, onMounted } from "vue";
+import { routes } from "@/router";
+import {SideMenu}
+export default defineComponent({
+  setup() {
+    onMounted(() => {
+      console.log(routes);
+    });
+
+    return {
+      menuList: ref(routes),
+      selectedKeys: ref(["1"]),
+      collapsed: ref(false),
+    };
+  },
+});
+</script>
+<style>
+.trigger {
+  float: left;
+  font-size: 18px;
+  line-height: 64px;
+  padding: 0 24px;
+  cursor: pointer;
+  transition: color 0.3s;
+}
+
+.trigger:hover {
+  color: #1890ff;
+}
+
+.logo {
+  height: 32px;
+  background: rgba(255, 255, 255, 0.3);
+  margin: 16px;
+}
+
+.site-layout .site-layout-background {
+  background: #fff;
+}
+</style>
---
+++ VUE3/src/components/SideMenu.vue
@@ -0,0 +1,38 @@
+<template>
+  <template v-for="(item, index) in menuList" :key="index">
+    <!-- 遍历路由表，生成左侧菜单 -->
+    <template v-if="item.children && item.children.length > 0">
+      <a-sub-menu>
+        <template #title>
+          <span>
+            <user-outlined />
+            {{ item.name }}
+          </span>
+        </template>
+
+        <a-menu-item
+          v-for="childItem in item.children"
+          :key="item.children + '/' + childItem.path"
+          >{{ childItem.name }}</a-menu-item
+        >
+      </a-sub-menu>
+    </template>
+    <template v-else>
+      <a-menu-item>
+        <span>{{ item.name }}</span>
+      </a-menu-item>
+    </template>
+  </template>
+</template>
+
+<script>
+import { defineComponent, ref, onMounted } from "vue";
+
+export default defineComponent({
+  setup() {
+    onMounted(() => {});
+
+    return {};
+  },
+});
+</script>
